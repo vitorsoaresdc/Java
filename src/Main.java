4230042a5a0d84debfbff3b8ad5b04f5378c9a6d
Vitor Soares <vitorsoaresdc@gmail.com>
2023-01-15 23:48:02 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,17 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Funcionario func1 = new Funcionario("vitor", 25, "123.678.546-56");
+        Scanner entrada = new Scanner(System.in);
 
-        Funcionario func2 = new Funcionario("Neia", 54, "123456");
+        System.out.print("Digite seu nome: ");
 
-        func2.testaConta();
+        String nome = entrada.nextLine();
+        System.out.println(nome);
 
-        func2.comer();
-        func2.dormir();
-
-        System.out.println("--------------------ArrayList--------------------");
-
-        ArrayList<String> clientes = new ArrayList<>();
-        clientes.add("Vitor");
-        clientes.add("Dulce");
-        clientes.add("neia");
-        clientes.add("nina");
-        clientes.add("Kelly");
-        clientes.add("Xena");
-
-        clientes.remove(0);
-        //clientes.clear();
-        System.out.println(clientes.size());
-        System.out.println(clientes.isEmpty());
-
-
-        System.out.println(clientes);
-
+        entrada.close();
     }
 
 }
